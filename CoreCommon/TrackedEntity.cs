d44eeb79c5104ae940d54dbd52715b4e11878f8f
msafarigit <msafari.int@gmail.com>
2018-05-27 17:38:00 +0430

    TrackedEntity

---
+++ CoreCommon/TrackedEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCommon
+{
+    public class TrackedEntity : Entity
+    {
+        public int InsertUserID { get; set; }
+        public string InsertUserName { get; set; }
+        public DateTime InsertDate { get; set; }
+        public int? UpdateUserID { get; set; }
+        public string UpdateUserName { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+}
